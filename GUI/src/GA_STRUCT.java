4896af2ec767dbe54a50de8fb9eb995c233757fe
petroskem <petroskem@cwu.edu>
2017-01-10 18:20:54 -0800

    Update GA_STRUCT.java

--- GUI/src/GA_STRUCT.java
+++ GUI/src/GA_STRUCT.java
@@ -18,8 +18,8 @@ public class GA_STRUCT {
 	// Some sort of structure like this might allow for some comparisons.
 	/*
 	 * For example: 
-	 * if (GA_STRUCT.availability.containsKey(class_date)
-	 * 		if (GA_STRUCT.availability.get(class_date) == null)
+	 * if (GA_STRUCT.availability.containsKey(class_date) // Checking that the GA is does not have any other obligations at this time
+	 * 		if (GA_STRUCT.availability.get(class_date) == null) // Checking that the GA hasn't already been assigned a class for this time
 	 * 			ASSIGN_GA_AS_TA(class_date);	 * 
 	 */
 	
