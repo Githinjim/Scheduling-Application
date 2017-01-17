d0649395a5214c4186a5309949a32fbbc7e4051a
petroskem <petroskem@cwu.edu>
2017-01-17 15:54:02 -0800

    Simple Apache POI Example:
    Provided a simple example of using a JavaFX FileChooser to select an
    Excel file. The file is read in, a new sheet is created and a value is
    written to the cell at (0,0).

--- TA_Scheduler_FX/src/application/FXMLDocumentController.java
+++ TA_Scheduler_FX/src/application/FXMLDocumentController.java
@@ -5,14 +5,22 @@
  */
 package application;
 
+import java.awt.Desktop;
 import java.io.File;
+import java.io.FileInputStream;
+import java.io.FileOutputStream;
 import java.net.URL;
 import java.util.List;
 import java.util.ResourceBundle;
+
+import org.apache.poi.ss.usermodel.Cell;
+import org.apache.poi.xssf.usermodel.XSSFRow;
+import org.apache.poi.xssf.usermodel.XSSFSheet;
+import org.apache.poi.xssf.usermodel.XSSFWorkbook;
+
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
 import javafx.fxml.Initializable;
-import javafx.scene.control.Label;
 import javafx.stage.FileChooser;
 import javafx.stage.FileChooser.ExtensionFilter;
 
@@ -21,16 +29,36 @@ import javafx.stage.FileChooser.ExtensionFilter;
  * @author Matthew
  */
 public class FXMLDocumentController implements Initializable {
-    FileChooser chooser = new FileChooser();
+    FileChooser fileChooser = new FileChooser();
+    private Desktop desktop = Desktop.getDesktop();
+    
     @FXML
-    private void handleButtonAction(ActionEvent event) {
-        chooser.showOpenMultipleDialog(null);
+    private void handleButtonAction(ActionEvent event) throws Exception {
+        List<File> list =
+                fileChooser.showOpenMultipleDialog(null);
+            if (list != null) {
+                for (File file : list) {
+                    FileInputStream fIP = new FileInputStream(file);
+                    XSSFWorkbook workbook = new XSSFWorkbook(fIP);
+                    XSSFSheet spreadsheet = workbook.createSheet();
+                    XSSFRow row = spreadsheet.createRow(0);
+                    Cell cell = row.createCell(0);
+                    cell.setCellValue("test");
+                    
+                    FileOutputStream fOP = new FileOutputStream(file);
+                    workbook.write(fOP);
+                    
+                    fOP.close();
+                	
+                }
+            }
     }
+    
     
     @Override
     public void initialize(URL url, ResourceBundle rb) {
-        chooser.getExtensionFilters().add(new ExtensionFilter("Excel Files (*.xlsx)", "*.xlsx", ".xls"));
-        chooser.getExtensionFilters().add(new ExtensionFilter("All Files", "*.*"));
+    	fileChooser.getExtensionFilters().add(new ExtensionFilter("Excel Files (*.xlsx)", "*.xlsx", ".xls"));
+    	fileChooser.getExtensionFilters().add(new ExtensionFilter("All Files", "*.*"));
         
     }    
     
