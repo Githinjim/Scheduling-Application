72e0e69a48dcbf4ead135849584c75cbbc995dde
Aaron Ritter <Aaron Ritter@AaronRitter-HP>
2017-01-29 15:50:53 -0800

    Still testing...

--- TA_Scheduler_FX/src/application/FXMLDocumentController.java
+++ TA_Scheduler_FX/src/application/FXMLDocumentController.java
@@ -221,7 +221,7 @@ public class FXMLDocumentController implements Initializable {
 	    {
 	    	resultsText.appendText("Loading Classes.\n");
 	    	//load in classes... >_>
-	    	
+	    	System.out.println("test");
 	    	List<File> classes =
 	                fileChooser.showOpenMultipleDialog(null);
 	    	
