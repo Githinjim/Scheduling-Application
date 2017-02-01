0f2586af256132e0bfb8851c30492d5a0a7dc5d4
petroskem <petroskem@cwu.edu>
2017-01-31 17:39:01 -0800

    Adding functionality for running the algorithm from the GUI

--- TA_Scheduler_FX/src/application/FXMLDocumentController.java
+++ TA_Scheduler_FX/src/application/FXMLDocumentController.java
@@ -349,45 +349,9 @@ public class FXMLDocumentController implements Initializable {
 
 	@FXML
 	private void runAlgorithm(ActionEvent event) {
-		// Fake creation of GAs
-		GraduateAssistant student1 = new GraduateAssistant("bob", "123-456-7890");
-		student1.setAvailableAt(0, "8am");
-
-		GraduateAssistant student2 = new GraduateAssistant("foo", "bar");
-		student2.setAvailableAt(0, "8am");
-		student2.setAvailableAt(1, "8am");
-
-		String startTime = "8am";
-		String endTime = "9am";
-
-//		// Fake creation of Class
-//		Class class1 = new Class();
-//		class1.setClassNumber("123");
-//		class1.addDayOfWeek(0);
-//		class1.setStartTime(startTime);
-//		class1.setEndTime(endTime);
-//
-//		Class class2 = new Class();
-//		class2.setClassNumber("456");
-//		class2.addDayOfWeek(1);
-//		class2.setStartTime(startTime);
-//		class2.setEndTime(endTime);
-//
-//		Algorithm schedule = new Algorithm();
-//		schedule.addClass(class1);
-//		schedule.addClass(class2);
-//		schedule.addGradStudent(student1);
-//		schedule.addGradStudent(student2);
-//
-//		schedule.initializeGraph();
-//		schedule.createInitialSolution();
-//
-//		System.out.println(class2.getAssignedGA().getName());
-//		 System.out.println(student1.getAssignedClasses().get(0).getClassNumber());
-
-		// Demo of determining if the student is available for a certain class.
-		// System.out.println(student.isAvailable(myClass.getDaysOfWeek(),
-		// myClass.getStartTime(), myClass.getEndTime()));
+		Algorithm alg = new Algorithm(gradList, classList);
+		alg.initializeGraph();
+		alg.createInitialSolution();
 	}
 
 	@Override
